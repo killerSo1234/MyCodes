b81cd498450c318d739e0a5bf269e08e3b9ad2e0
PRIYANSHU KUMAR SINGH <49274049+PKSingh0017@users.noreply.github.com>
2019-10-20 21:08:26 +0530

    Final Task
    A c++ code for a problem from hacker earth.

---
+++ finaltask.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+
+int main()
+{
+    int N, i, max=1, res=0;
+    cin >> N;
+    int a[N];
+    for(i=0 ; i<N ; i++)
+    {
+        cin >> a[i];
+        res = gcd(a[i], res);
+        if(res == 1)
+        {
+            break;
+        }
+    }
+    cout << (res*N) << "\n";
+    return 0;
+}
